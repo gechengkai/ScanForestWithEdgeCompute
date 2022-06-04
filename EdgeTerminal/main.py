72e321814a5148f285c771186ac46f51c6e128f6
gechengkai <gechengkai@126.com>
2022-06-04 22:17:12 +0800

    指定服务器的具体地址

--- EdgeTerminal/main.py
+++ EdgeTerminal/main.py
@@ -2,7 +2,7 @@ import socket
 
 
 server = socket.socket(socket.AF_INET, socket.SOCK_STREAM) #创建套接字： ipv4 , TCP连接
-host = ''
+host = '192.168.31.69'
 port = 5150
 server.bind((host, port))# 绑定地址与端口
 server.listen(5)# 监听
