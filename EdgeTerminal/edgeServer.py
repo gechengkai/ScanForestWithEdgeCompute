b50c472ebfa0b4b55f2f20e6d95c5dbc373ddefc
gechengkai <gechengkai@126.com>
2022-06-05 16:29:07 +0800

    修改edgeServer.py

--- EdgeTerminal/edgeServer.py
+++ EdgeTerminal/edgeServer.py
@@ -8,22 +8,20 @@ ip_port=("192.168.31.69",19984)
  
 class MyServer(socketserver.BaseRequestHandler):
     def handle(self):
-        print("conn is :",self.request) # 连接句柄
-        print("addr is :",self.client_address) # IP地址
+        userClinet = self.request
+        userAddr = self.client_address# IP地址
+        print("Accepted connection from: ", userAddr) 
         while True:
             try:
-                self.str = self.request.recv(8)
-                data = bytearray(self.str)
-                if (bytes.decode(data)!='exit'):
+                data = userClinet.recv(1024)
+                if (bytes.decode(data) =='exit'):
                     break
-                else :
+                else:
                     print('Received data form UserTerminal:  ', bytes.decode(data))    
-                    self.request.send(str.encode('连接成功！')) #给用户端一个连接状态回复
+                    userClinet.send(str.encode('连接成功！')) #给用户端一个连接状态回复
             except Exception as e:
                 print(e)
-                break
- 
- 
+                break   
 if __name__ == "__main__":
     s = socketserver.ThreadingTCPServer(ip_port, MyServer)
     print("start listen")
