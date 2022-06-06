02ba27c9a70d7d787620115759c2723a978cd8f1
gechengkai <gechengkai@126.com>
2022-06-06 11:14:36 +0800

    修改edgeServer.py中的client拼写

--- EdgeTerminal/edgeServer.py
+++ EdgeTerminal/edgeServer.py
@@ -1,37 +1,40 @@
+from mimetypes import init
 import socketserver
 import os
 import sys
 import time
 import threading
  
-ip_port=("192.168.31.69",19984)
  
 class ClientServer(socketserver.BaseRequestHandler):
     def handle(self):
-        userClinet = self.request
+        userClient = self.request
         userAddr = self.client_address# IP地址
         print("Accepted connection from: ", userAddr) 
         while True:
             try:
-                data = userClinet.recv(1024)
+                data = userClient.recv(1024)
                 if (bytes.decode(data) =='connect'):
                     print("CONNECT ORDER!")
-                    userClinet.send(str.encode('连接成功！')) #给用户端一个连接状态回复
+                    userClient.send(str.encode('连接成功！')) #给用户端一个连接状态回复
                     break
                 elif (bytes.decode(data) =='scan'):
                     print("SCAN ORDER!")
-                    userClinet.send(str.encode('开始扫描！')) #给用户端一个连接状态回复
+                    userClient.send(str.encode('开始扫描！')) #给用户端一个连接状态回复
                     break
                 else:
                     print('Received data form UserTerminal:  ', bytes.decode(data))    
-                    userClinet.send(str.encode('收到！')) #给用户端一个连接状态回复
+                    userClient.send(str.encode('收到！')) #给用户端一个连接状态回复
                     break
             except Exception as e:
                 print(e)
                 break   
-        userClinet.close()
-        print("Close connection of :",userClinet)
+        userClient.close()
+        print("Close connection of :",userClient)
+
+        
 if __name__ == "__main__":
+    ip_port=("192.168.31.69",19984)
     s = socketserver.ThreadingTCPServer(ip_port, ClientServer)
     print("start listen")
     s.serve_forever()
